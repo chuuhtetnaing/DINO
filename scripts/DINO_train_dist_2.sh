0ef602c3c5fc07c50836f02630a1a05dcab573c3
chuuhtetnaing <chuuhtetnaing@gmail.com>
2023-08-20 01:34:38 +0800

    Update DINO_train_dist_2.sh

--- scripts/DINO_train_dist_2.sh
+++ scripts/DINO_train_dist_2.sh
@@ -3,4 +3,4 @@ python -m torch.distributed.launch --nproc-per-node=2 main.py \
 	--output_dir logs/DINO/R50-MS4 -c config/DINO/DINO_4scale.py --coco_path $coco_path \
 	--options dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
-	dn_box_noise_scale=1.0
+	dn_box_noise_scale=1.0 --num_workers 4
